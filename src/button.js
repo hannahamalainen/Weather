9d84048e3cf4c3df3ba530d8b009526fb6043886
Hanna Hämäläinen <hanna.hamalainen@aalto.fi>
2017-12-27 12:35:14 +0200

    adding all files

---
+++ src/button.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Button extends Component {
+    render() {
+        return(
+        <h2>{this.props.text}</h2>
+        )
+    }
+}
+
+export default Button;
---
+++ src/kokeilu.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class NewClass extends Component {
+    render() {
+        return(
+            <h1>Moikkamoi</h1>
+        )
+    }
+}
+
+export default NewClass;
